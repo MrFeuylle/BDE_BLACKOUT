eb10bdcc99cb747d4484067843c387c1c1abad0c
Axel Giguaire <113340829+MrFeuylle@users.noreply.github.com>
2024-03-20 10:32:21 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-def create_video_from_images(image_folder, output_video, fps=30, delay_between_frames=100):
+def create_video_from_images(image_folder, output_video, fps, delay_between_frames):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
@@ -15,10 +15,9 @@ def create_video_from_images(image_folder, output_video, fps=30, delay_between_f
     cv2.destroyAllWindows()
     video.release()
 
-# Usage example:
 image_folder = "./image"
 output_video = "./bde.mp4"
-fps = 1  # Frames per second
-delay_between_frames = 15000  # Delay between frames in milliseconds
+fps = 0.1
+delay_between_frames = 15000
 
 create_video_from_images(image_folder, output_video, fps, delay_between_frames)
