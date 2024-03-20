aad22068edc790148cceb9644e090b9fae143a50
Axel Giguaire <113340829+MrFeuylle@users.noreply.github.com>
2024-03-20 13:58:03 +0100

    Fix fill whit black for resize

--- script.py
+++ script.py
@@ -1,10 +1,21 @@
 import cv2
 import os
+import numpy as np
 
-def resize_image(image, target_width, target_height):
-    return cv2.resize(image, (target_width, target_height))
+def resize_with_black(image, new_width, new_height):
+    original_height, original_width = image.shape[:2]
+    scale_x = new_width / original_width
+    scale_y = new_height / original_height
+    scale = min(scale_x, scale_y)
+    resized_image = cv2.resize(image, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+    black_image = np.zeros((new_height, new_width, 3), dtype=np.uint8)
+    start_x = (new_width - int(original_width * scale)) // 2
+    start_y = (new_height - int(original_height * scale)) // 2
+    black_image[start_y:start_y + resized_image.shape[0], start_x:start_x + resized_image.shape[1]] = resized_image
 
-def create_video_from_images(image_folder, output_video, fps=30, duration_per_image=2, target_width=None, target_height=None):
+    return black_image
+
+def create_video_from_images(image_folder, output_video, fps, duration_per_image, target_width=None, target_height=None):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     
     frame = cv2.imread(os.path.join(image_folder, images[0]))
@@ -15,18 +26,20 @@ def create_video_from_images(image_folder, output_video, fps=30, duration_per_im
     if target_height is None:
         target_height = height
 
-    video = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'mp4v'), fps, (target_width, target_height))
+    frames_per_image = int(fps * duration_per_image)
+    video = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'MJPG'), fps, (target_width, target_height))
 
     for image in images:
         frame = cv2.imread(os.path.join(image_folder, image))
-        frame = resize_image(frame, target_width, target_height)
-        video.write(frame)
+        frame = resize_with_black(frame, target_width, target_height)
+        for _ in range(frames_per_image):
+            video.write(frame)
 
     cv2.destroyAllWindows()
     video.release()
 image_folder = "./image"
-output_video = "./bde.mp4"
-fps = 0.1
-delay_between_frames = 15000
+output_video = "./bde.mkv"
+fps = 1
+delay_between_frames = 5
 
 create_video_from_images(image_folder, output_video, fps, delay_between_frames)
